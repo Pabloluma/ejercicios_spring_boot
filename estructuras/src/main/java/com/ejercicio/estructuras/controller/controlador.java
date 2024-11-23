dbcb07dd6b8c1f0c98d0e0e9f87702b8d2780e9c
Pablo <pablolupionmarquez@gmail.com>
2024-11-23 19:48:04 +0100

    Ejercicios completos

--- estructuras/src/main/java/com/ejercicio/estructuras/controller/controlador.java
+++ estructuras/src/main/java/com/ejercicio/estructuras/controller/controlador.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.util.Random;
 
 @RestController
 public class controlador {
@@ -63,9 +63,60 @@ public class controlador {
             categoria = "obesidad";
         }
         return "El usuario tiene " + categoria + " su IMC es " + calcula;
-
+    }
+    @PostMapping("/encuesta")
+    String encuesta(@RequestParam String satisfaccion) {
+        return "La encuesta de satisfaccion es " + satisfaccion;
     }
 
+    @PostMapping("/generarContrasena")
+    String generarContrasena(@RequestParam int longitud) {
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()_-+=<>?";
+      Random random = new Random();
+        String contrasena = "";
+        for (int i = 0; i < longitud; i++) {
+//      Obtiene el indice de un caracter aleatorio
+            int indice = random.nextInt(caracteres.length());
+//      Añade la letra al String cadena
+            contrasena += caracteres.charAt(indice);
+        }
+        return contrasena;
+    }
 
+    @PostMapping("/sumatoria")
+    String sumatorio(@RequestParam int numero) {
+        int suma = 0;
+        for (int i = 1; i < numero+1 ; i++) {
+            suma += i;
+        }
+        return "El sumatorio es " + suma;
+    }
+
+    @PostMapping("/factorial")
+    String factorial(@RequestParam int numero) {
+        String[] numeroF = resuelveFactorial(numero);
+        return "El factorial es " + numeroF[1] + "\n" + numeroF[0];
+    }
+
+String[] resuelveFactorial(int numero) {
+    int factorial = 1;
+    String valores = "";
+    for (int i = 1; i <= numero; ++i) {
+        factorial *= i;
+        if (i == numero) {
+            valores +=  i + " = " + factorial;
+        }else if (i == 1){
+            valores += "<p>" + i + " * ";
+        }else{
+            valores += i + " * ";
+        }
+
+    }
+    valores += "</p";
+    String[] resultado = new String[2];
+    resultado[0] = valores;  // Convertimos el resultado a String
+    resultado[1] = factorial + "";
+    return resultado;
+}
 
 }
